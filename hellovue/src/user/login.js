75b0e2c4579a36548088433e3d2e52834f761854
myhappinesszhang <2189247923@qq.com>
2021-12-20 20:11:20 +0800

    Initial commit

---
+++ hellovue/src/user/login.js
@@ -0,0 +1,10 @@
+const loginForm={
+    template:'\
+         <div>\
+         <h2>登入界面</h2>\
+         用户名：<input type="text"><br>\
+         密码：<input type="password"><br>\
+         <input type="submit">\
+         </div>\
+       '
+};
